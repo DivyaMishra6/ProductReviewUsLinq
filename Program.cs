7ac47a14dba20b1759ac5ed73cb113ff3e7e3082
DivyaMishra6 <divyamishra599@gmail.com>
2023-07-17 22:07:28 +0530

    [Divya]: Create Mode List And Add Value

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿
+namespace ProjectManagmentLinq
+{
+    public class Program
+    {
+        public static void Main(String[] args)
+        {
+            Console.WriteLine("Welcome to Product Review Managment Program!!!");
+
+            List<ProductReview> ProductReviewList = new List<ProductReview>()
+            {
+                new ProductReview(){Productid = 1,UserID = 10,Rating=5,Review="Good",IsLike=true},
+                new ProductReview(){Productid = 2,UserID = 11,Rating=4,Review="Nice",IsLike=true},
+                new ProductReview(){Productid = 3,UserID = 12,Rating=3,Review="Average",IsLike=true},
+                new ProductReview(){Productid = 4,UserID = 13,Rating=4,Review="Nice",IsLike=true},
+                new ProductReview(){Productid = 5,UserID = 14,Rating=5,Review="Good",IsLike=true}
+
+            };
+        }
+    }
+}

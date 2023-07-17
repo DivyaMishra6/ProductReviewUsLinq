44ca24285fa3382a3110c77aca475128e54d58c7
DivyaMishra6 <divyamishra599@gmail.com>
2023-07-17 22:34:17 +0530

    [Divya]: Code for top three records

---
+++ Managemenent.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectManagmentLinq
+{
+    public class Managemenent
+    {
+        public static void Top3Records(List<Productreview> listobj)
+        {
+            var Product = (from product in listobj orderby product.rating descending select product).Take(3);
+
+            foreach (var product in Product)
+            {
+                Console.WriteLine("*************");
+                Console.WriteLine(product.ProductID + " | " + product.Userid + " | " + product.rating + " | " + product.Review + "  | " + product.Islike);
+                Console.WriteLine("*************");
+            }
+        }
+    }
+}

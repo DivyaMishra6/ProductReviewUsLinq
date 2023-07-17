145f3940121b1bb60c025992d126b48456060d28
DivyaMishra6 <divyamishra599@gmail.com>
2023-07-17 22:23:03 +0530

    [Divya]: Create Mode List And Add Value

--- Program.cs
+++ Program.cs
@@ -5,17 +5,31 @@ namespace ProjectManagmentLinq
     {
         public static void Main(String[] args)
         {
-            Console.WriteLine("Welcome to Product Review Managment Program!!!");
 
-            List<ProductReview> ProductReviewList = new List<ProductReview>()
+            Console.WriteLine("Welcome to Linq Program");
+
+            List<Productreview> listobj = new List<Productreview>();
+
             {
-                new ProductReview(){Productid = 1,UserID = 10,Rating=5,Review="Good",IsLike=true},
-                new ProductReview(){Productid = 2,UserID = 11,Rating=4,Review="Nice",IsLike=true},
-                new ProductReview(){Productid = 3,UserID = 12,Rating=3,Review="Average",IsLike=true},
-                new ProductReview(){Productid = 4,UserID = 13,Rating=4,Review="Nice",IsLike=true},
-                new ProductReview(){Productid = 5,UserID = 14,Rating=5,Review="Good",IsLike=true}
-
+                new Productreview() { ProductID = 1, Userid = 1, rating = 3, Review = "good", Islike = true };
+                new Productreview() { ProductID = 2, Userid = 2, rating = 3, Review = "Wrost", Islike = false };
+                new Productreview() { ProductID = 2, Userid = 3, rating = 5, Review = "very good", Islike = true };
+                new Productreview() { ProductID = 4, Userid = 4, rating = 3, Review = "bad", Islike = false };
+                new Productreview() { ProductID = 6, Userid = 5, rating = 5, Review = "Excellent", Islike = true };
+                new Productreview() { ProductID = 6, Userid = 6, rating = 3, Review = "Wrost", Islike = false };
+                new Productreview() { ProductID = 6, Userid = 7, rating = 4, Review = "good", Islike = true };
+                new Productreview() { ProductID = 8, Userid = 8, rating = 5, Review = "Excellent", Islike = true };
+                new Productreview() { ProductID = 9, Userid = 9, rating = 3, Review = "bad", Islike = false };
+                new Productreview() { ProductID = 10, Userid = 10, rating = 5, Review = "Very good", Islike = true };
             };
+
+            foreach (var product in listobj)
+            {
+                Console.WriteLine("******************");
+                Console.WriteLine(product.ProductID + "  | " + product.Userid + "  | " + product.rating + " | " + product.Review + " | " + product.Islike);
+            }
+
+            
         }
     }
 }
